6d8ede7f7e4ebdeb0eec4c934684e074465745aa
Cameron Sampson <vaerros@gmail.com>
2013-07-20 23:35:00 -0500

    'randomize' feature and other changes
    - Removed a few ng-change bindings in favor of more elegant
      directive fu
    - Proverbs 'db' now consists of an array containing each
      individual proverb
    - Creation of chunks/paragaphs of proverbs, instead of being done
      beforehand on proverbs.json content is now done on the client-
      side.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,36 @@
 window.app = angular.module('LemanIpsum', []);
 
 app.controller('Generator', function($scope, $http) {
+  $scope.paragraphs = [];
   $scope.outputSize = 3;
   $scope.outputFormat = 'text';
 
-  $scope.updateSelection = function() {
+  function createParagraphs(proverbs) {
+    var paragraphs = [];
+
+    for(var i=0, l=proverbs.length; i<l; i++) {
+      if(i % 8 === 0) paragraphs.push(proverbs.slice(i, i+7).join(' '));
+    }
+
+    return paragraphs;
+  }
+
+  $scope.getSelection = function() {
     var selection = $scope.paragraphs.slice(0, $scope.outputSize);
 
     if($scope.outputFormat === 'html')
       selection = selection.map(function(p) { return '<p>'+p+'</p>'; });
 
-    $scope.selection = selection.join('\n\n');
+    return selection.join('\n\n');
+  };
+
+  $scope.randomizeSelection = function() {
+    var proverbs = _.shuffle($scope.proverbs);
+    $scope.paragraphs = createParagraphs(proverbs);
   };
 
   $http.get('/proverbs').success(function(response) {
-    $scope.paragraphs = response;
-    $scope.updateSelection();
+    $scope.proverbs = response;
+    $scope.paragraphs = createParagraphs(response);
   });
 });
